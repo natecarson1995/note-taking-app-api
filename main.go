8b14275ccd16d55662235074c6a96c5a293e087f
natecarson1995 <natecarson1995@gmail.com>
2021-06-16 14:48:45 -0400

    ^

--- main.go
+++ main.go
@@ -126,8 +126,8 @@ func main() {
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
-		c.Header("Access-Control-Allow-Origin", "http://10.0.0.41:5000")
-		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "false")
 		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Header("Access-Control-Allow-Methods", "POST, OPTIONS, GET, DELETE")
 
